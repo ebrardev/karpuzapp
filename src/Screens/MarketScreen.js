9dab6370d4581dad102cd54c4774629fd3c82c20
ebrardev <16155052@mersin.edu.tr>
2023-08-13 01:24:06 +0300

    market screen

--- src/Screens/MarketScreen.js
+++ src/Screens/MarketScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View,TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View,TouchableOpacity,Image } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -8,14 +8,74 @@ export default function MarketScreen() {
     const navigation = useNavigation();
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
+
+
+      <Image style={styles.image} source={require('../../assets/market.png')}  />
   
-      <TouchableOpacity onPress={() => navigation.navigate('Stand')}>
-        <Text>Meyve mağazasına gidiniz</Text>
+        <Text style={styles.frame}> Karpuz alacağımız markete gidiyoruz </Text>
+        <Text style={styles.small}> Karpuz reyonuna ilerliyoruz </Text>
+     
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Stand')}>
+        <Text  style={styles.text}>Sonraki Adım</Text>
         </TouchableOpacity>
     
+    <TouchableOpacity style={styles.back} onPress={() => navigation.navigate('Home')}>
+        <Text style={styles.text}>Önceki Adım </Text>
+        </TouchableOpacity>
         </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    
+    alignItems:'center'
+},
+image:{
+  marginTop:100,
+    width:300,
+    height:300,
+    borderRadius:10,
+},
+button:{
+    backgroundColor:'green',
+    padding:10,
+    borderRadius:10,
+    position:'absolute',
+    bottom:150,
+    right:10,
+    alignItems:'center',
+    color:'white'
+},
+text:{
+    color:'white',
+    fontWeight:'bold',
+    fontSize:16,
+    textAlign:'center'
+},
+back:{
+
+    backgroundColor:'red',
+    padding:10,
+    borderRadius:10,
+    position:'absolute',
+    bottom:150,
+    left:10,
+},
+frame:{
+    color:'black',
+    fontWeight:'bold',
+    fontSize:16,
+    textAlign:'center',
+    marginTop:15,
+},
+small:{
+    color:'grey',
+    fontWeight:'bold',
+    fontSize:18,
+    textAlign:'center',
+    marginTop:5,
+},
+})
